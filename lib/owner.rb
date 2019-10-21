84e9dbcd6ebbe50704d79ac29f6a36f28757134a
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-21 00:30:30 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,13 +1,15 @@
 class Owner
-  attr_reader :name, :species # code goes here
+  attr_reader :name, :species
+  #attr_accessor :all_cats, :all_dogs # code goes here
   
   @@all = []
-  @cats = []
-  @dogs = []
+  
   
   def initialize(name)
     @name = name
     @species = "human"
+    @all_cats = []
+    @all_dogs = []
     @@all << self
   end
   
@@ -25,5 +27,29 @@ class Owner
   
   def self.reset_all
     @@all.clear
+  end
+  
+  def cats
+    Cats.all.select {|cat| cat.owner == self}
+  end
+  
+  
+  
+  # def dogs
+  #   Dogs.all.select {|dog| dog.owner == self}
+  # end
+  
+  # def buy_cat(this_cat)
+    
+  #   @all_cats << this_cat
+  #   binding.pry
+  # end
+  
+  # def buy_dog(this_dog)
+  #   @all_dogs << this_dog
+  # end
+  
+  def self.find_or_create_by_name(name)
+    @@all.find{ |owner| owner.name == name } ||  self.new(name)
   end
 end
